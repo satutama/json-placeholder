8d1240cd373312623df5e8d9b4a8223e4180665f
Satria Utama <satria.u.s@gmail.com>
2023-12-27 18:13:05 +0100

    initial state fix

--- src/app/features/posts/state/posts.reducer.ts
+++ src/app/features/posts/state/posts.reducer.ts
@@ -1,11 +1,8 @@
 import { createReducer, on } from '@ngrx/store';
 import * as PostsActions from './posts.actions';
-import { PostsState } from './posts.state';
 
-export const initialState: PostsState = {
-  posts: {
-    isLoading: false,
-  },
+export const initialState = {
+  isLoading: false,
 };
 
 export const postsReducer = createReducer(
