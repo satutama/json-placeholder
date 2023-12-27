79938fa93fa875eea8f44e8699d17eedba2032a8
Satria Utama <satria.u.s@gmail.com>
2023-12-27 17:57:41 +0100

    define init actions

---
+++ src/app/features/posts/state/posts.actions.ts
@@ -0,0 +1,13 @@
+import { createAction, props } from '@ngrx/store';
+import { Post } from '../post/post';
+
+export const loadPosts = createAction('[Posts] Load Posts');
+export const loadPostsSuccess = createAction(
+  '[Posts] Load Posts Success',
+  props<{ posts: Post[] }>()
+);
+export const loadPostsFailure = createAction(
+  '[Posts] Load Posts Failure',
+  props<{ error: any }>()
+);
+export const selectNextPost = createAction('[Posts] Select Next Post');
