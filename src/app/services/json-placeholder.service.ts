e6c8db6af2da1dad7e957436465094bb91c0dad8
Satria Utama <satria.u.s@gmail.com>
2023-12-27 18:01:46 +0100

    update service

--- src/app/services/json-placeholder.service.ts
+++ src/app/services/json-placeholder.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../features/posts/post/post';
-import { Loadable } from '../utils/loadable';
-import { mapToLoadable } from '../utils/mapToLoadable';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +11,7 @@ export class JsonPlaceholderService {
 
   constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<Loadable<Post[]>> {
-    return this.http.get<Post[]>(`${this.url}/posts`).pipe(mapToLoadable);
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.url}/posts`);
   }
 }
